c4af5834498ee795ad543b26845d691a52b9ce4a
Gizex <admin@gizex.ru>
2023-10-07 00:15:52 +0300

    change /usr/local/bin/prometheus_wireguard_exporter dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Start prometheus_wireguard_exporter in the background
-/usr/local/bin/prometheus_wireguard_exporter -p 51822 -s true -r true -d true &
+/app -p 51822 -s true -r true -d true &
 # Start your Node.js application
 exec node server.js
