682a3d827f7356a36730369aef5ca62d6d06d4bd
Gizex <admin@gizex.ru>
2023-10-07 01:00:53 +0300

    fix/add .conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Start prometheus_wireguard_exporter in the background
-/app/prometheus_wireguard_exporter -p 51822 -s true -r true -d true -n /etc/wireguard/${WG_INTERFACE} &
+/app/prometheus_wireguard_exporter -p 51822 -s true -r true -d true -n /etc/wireguard/${WG_INTERFACE}.conf &
 # Start your Node.js application
 exec node server.js
